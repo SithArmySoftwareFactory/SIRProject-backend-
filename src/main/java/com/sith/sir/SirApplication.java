e91b02880ccf58b90461a200f12bb25cde6cd421
Joshua Matos <josh@joshuamatos.com>
2022-06-03 14:00:57 -0500

    Twilo added

--- src/main/java/com/sith/sir/SirApplication.java
+++ src/main/java/com/sith/sir/SirApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SirApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SirApplication.class, args);
+
+
     }
 
 }
+
+
