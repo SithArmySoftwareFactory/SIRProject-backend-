63515a231a9d4243c901e366fba4d8c5867769a1
Joshua Matos <josh@joshuamatos.com>
2022-06-03 15:43:15 -0500

    Added new field, to track sentiment

--- src/main/java/com/sith/sir/IncidentService.java
+++ src/main/java/com/sith/sir/IncidentService.java
@@ -105,6 +105,9 @@ public class IncidentService {
             if(incident.getLng() != null)
             updatingIncident.setLng(incident.getLng());
 
+            if(incident.getSentiment() != null)
+                updatingIncident.setSentiment(incident.getSentiment());
+
 
             incidentRepository.save(updatingIncident);
 
