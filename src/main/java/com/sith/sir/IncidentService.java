ef1878bf2b498c8fb12a6cd9860b56941a341882
lanceredfearn <lance.c.redfearn.mil@swf.army.mil>
2022-06-09 18:19:07 -0500

    pulled current development branch and updated email

--- src/main/java/com/sith/sir/IncidentService.java
+++ src/main/java/com/sith/sir/IncidentService.java
@@ -26,7 +26,7 @@ public class IncidentService {
     public void sendMail(Incident incident) {
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        senderService.sendEmail("lance.c.redfearn.mil@gmail.com", String.format("**New Incident** %s %s" , formatter.format(date), incident.getDescription()) ,
+        senderService.sendEmail("lance.c.redfearn.mil@swf.army.mil", String.format("**New Incident** %s %s" , formatter.format(date), incident.getDescription()) ,
                 String.format("Sir/Ma'am,\n\n"+
                         "Below is the information for a new incident. Please log into the SIR portal to review.\n\n"+
                         "Location of Event: %s\n\n" +
